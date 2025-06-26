705c6ef04ee5590cc27d24fbd1785670e5a6fc1c
jazmynnbanks <jazmynn.lee@outlook.com>
2025-06-26 12:40:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+Dictionary<int, int> counts = new Dictionary<int, int>();
+int majorityCount = nums.Length / 2;
+int majorityElement = 0;
+
+for (int i = 0; i < nums.Length; i++)
+{
+    if (counts.ContainsKey(nums[i]))
+    {
+        counts[nums[i]]++;
+    }
+    else
+    {
+        counts[nums[i]] = 1;
+    }
+    if (counts[nums[i]] > majorityCount)
+    {
+        majorityElement = nums[i];
+        break;
+    }
+}
+
+Console.WriteLine(majorityElement);
+Console.ReadKey();
